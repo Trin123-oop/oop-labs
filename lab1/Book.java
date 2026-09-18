4a4ddbbdab1861919f0c10ff874113144044a0a4
Trin <6930613010@psu.ac.th>
2026-09-18 12:34:03 +0700

    add Book

---
+++ lab1/Book.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Book {
+        private String title;
+        private String author;
+        private double price;
+        
+        public Book (String title, String name, double price){
+            this.title = title;
+            author = name;
+            this.price = price;
+        }
+        public void setTitle(String title){
+            this.title = title;
+        }
+        public void setAuthor(String author){
+            this.author = author;
+        }
+
+    }
+
